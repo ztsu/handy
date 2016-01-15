921eec6a7a0fa23af64abf8c3a5140f643855903
Alexey Golovnya <alexey.golovnya@gmail.com>
2016-01-15 21:48:54 +0600

    Add script for deploy to gh-pages

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd dist/
+cp ../index.html ./
+
+git init
+git config user.name "Alexey Golovnya"
+git config user.email "alexey.golovnya@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@${GITHUB_REP}" master:master > /dev/null 2>&1
