1035a94666a0326baa5369feef7f5c45ff32f15c
Alexey Golovnya <alexey.golovnya@gmail.com>
2016-03-10 23:11:54 +0600

    Reverse swipes

--- src/components/Word.jsx
+++ src/components/Word.jsx
@@ -12,7 +12,7 @@ export default class Word extends React.Component {
     const { word, ipa, translations, open, onTap, onSwipeLeft, onSwipeRight } = this.props;
 
     return (
-      <Swipeable onTap={onTap} onSwipe={event => event.velocityX < 0 ? onSwipeLeft() : onSwipeRight()}>
+      <Swipeable onTap={onTap} onSwipe={event => event.velocityX > 0 ? onSwipeLeft() : onSwipeRight()}>
         <ul className="handy-word" onMouseEnter={e => this.handleMouseEnter()}>
           <li className={["handy-word-item", "_ipa", open ? "" : "_hidden"].join(" ")}>{ipa}</li>
           <li className={["handy-word-item", "_word"].join(" ")}>{word}</li>
